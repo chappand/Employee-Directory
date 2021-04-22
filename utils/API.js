b37b9a0997b6bf478bd40407af761b0c61920182
Andrew Chappell <chappand@gmail.com>
2021-04-21 22:30:58 -0400

    adjustment to api.

--- utils/API.js
+++ utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getRandomDog: function() {
-      return axios.get("https://randomuser.me/");
+      return axios.get("https://randomuser.me/api/?results=50&nat=us");
     }
   };
